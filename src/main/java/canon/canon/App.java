11faba5067c8747107e2e7f707ee689c8dad8a7e
PPujari <PPujari@SILICUS520.silicus.com>
2016-10-06 19:04:25 +0530

    Commit and push

---
+++ src/main/java/canon/canon/App.java
@@ -0,0 +1,13 @@
+package canon.canon;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
